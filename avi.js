12346e9081d0f826b4038629cb314cd838772ef0
Toukir <toukerahmed.2728@gmail.com>
2025-07-28 22:12:52 +0600

    commit

---
+++ avi.js
@@ -0,0 +1,51 @@
+console.log("hi avii");
+
+// variable
+ var name="aviiiiiiiiii";
+ console.log(name);
+
+ var age=34;
+ console.log(age);
+
+ var year= 2009;
+ console.log(year);
+
+ year=1229;
+ console.log(year);
+ age=1299;
+ console.log(age);
+
+//  variables types  and type of 
+
+var username="avi_1229";
+var age = 22;
+var issingle = false;
+var salary= 45000.78;
+
+
+console.log( username);
+console.log( age);
+console.log( issingle);
+console.log( salary);
+
+console.log(typeof username);
+console.log(typeof age);
+console.log(typeof issingle);
+console.log(typeof salary);
+
+
+// 
+
+var data ='20000';
+console.log(data);
+console.log( typeof data);
+var data1 = parseInt(data);
+console.log(typeof data1);
+console.log(data1);
+
+var f1= 0.1;
+var f2= 0.2;
+var total= f1+f2;
+console.log(total);
+console.log(total.toPrecision(2));
+console.log(total.toFixed(2));
