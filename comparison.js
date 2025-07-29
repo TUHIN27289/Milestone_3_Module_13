69f9f7ca1b39e8e9f8df84495f3ebba601d8ba64
Toukir <toukerahmed.2728@gmail.com>
2025-07-29 17:24:52 +0600

    module-12 done

---
+++ comparison.js
@@ -0,0 +1,86 @@
+var applePrice=100;
+var orangePrice=80;
+var appleQuantity=2;
+orangeQuantity=5;
+const isGood=true;
+//  console.log(applePrice<orangePrice);
+//  console.log(applePrice>orangePrice);
+//  console.log(applePrice==orangePrice);
+//  console.log(applePrice!=orangePrice);
+//  console.log(applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice);
+//  console.log(applePrice===orangePrice);
+//  console.log(applePrice!==orangePrice);
+//  console.log(applePrice<=orangePrice && applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice || applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice && applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice || applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice && applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice || applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice && applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice || applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice && applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice || applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice && applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice || applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice && applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice || applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice && applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice || applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice && applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice || applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice && applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice || applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice && applePrice>=orangePrice);
+//  console.log(applePrice<=orangePrice || applePrice>=orangePrice);
+
+//  if else statement //
+
+
+// if(applePrice<orangePrice){
+//     console.log('apple price is less than orange price');
+// }
+// else{
+//     console.log('apple price is greater than orange price');
+// }
+
+
+// multiple logical operator
+
+// if(applePrice<orangePrice && appleQuantity==2 && isGood==true){
+//     console.log('pack apple');
+// }
+// else{
+//     console.log('do not pack apple');
+// }
+
+
+// complex condition
+
+// if((applePrice<orangePrice && appleQuantity==2 && isGood==true) || (applePrice>orangePrice && appleQuantity==2 && isGood==true)){
+//     console.log('pack apple');
+// }else
+// {
+//     console.log('do not pack apple');
+// }
+
+
+
+// multiple if-else
+
+
+
+//      let age=20;
+// if(age>=18){
+//     console.log('you are adult');
+// }
+// else if(age>=13 && age<18){
+//     console.log('you are a teenager');
+// }
+// else if(age>=6 && age<13){
+//     console.log('you are a child');
+// }
+// else
+// {
+//     console.log('you are a baby');
+// }
